624070786a3f5885849623bb9bfb896d6b548f64
Nathan Robertson <nathanrobertson1997@gmail.com>
2018-08-20 11:17:16 +1000

    Updated GUI.

--- ClassroomRobots/ClassroomRobots/Main.Designer.cs
+++ ClassroomRobots/ClassroomRobots/Main.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.Class_Label = new System.Windows.Forms.Label();
+            this.Teacher_Text = new System.Windows.Forms.TextBox();
+            this.Teacher_Label = new System.Windows.Forms.Label();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
             this.New = new System.Windows.Forms.ToolStripMenuItem();
@@ -40,16 +43,52 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.Exit = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.Class_Label);
+            this.panel1.Controls.Add(this.Teacher_Text);
+            this.panel1.Controls.Add(this.Teacher_Label);
             this.panel1.Location = new System.Drawing.Point(-1, 26);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(990, 50);
             this.panel1.TabIndex = 0;
+            // 
+            // Class_Label
+            // 
+            this.Class_Label.AutoSize = true;
+            this.Class_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Class_Label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Class_Label.Location = new System.Drawing.Point(319, 14);
+            this.Class_Label.Name = "Class_Label";
+            this.Class_Label.Size = new System.Drawing.Size(68, 25);
+            this.Class_Label.TabIndex = 2;
+            this.Class_Label.Text = "Class:";
+            // 
+            // Teacher_Text
+            // 
+            this.Teacher_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Teacher_Text.Location = new System.Drawing.Point(106, 9);
+            this.Teacher_Text.Name = "Teacher_Text";
+            this.Teacher_Text.Size = new System.Drawing.Size(192, 30);
+            this.Teacher_Text.TabIndex = 3;
+            // 
+            // Teacher_Label
+            // 
+            this.Teacher_Label.AutoSize = true;
+            this.Teacher_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Teacher_Label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Teacher_Label.Location = new System.Drawing.Point(13, 12);
+            this.Teacher_Label.Name = "Teacher_Label";
+            this.Teacher_Label.Size = new System.Drawing.Size(91, 25);
+            this.Teacher_Label.TabIndex = 2;
+            this.Teacher_Label.Text = "Teacher:";
             // 
             // toolStrip1
             // 
@@ -83,44 +122,44 @@
             // New
             // 
             this.New.Name = "New";
-            this.New.Size = new System.Drawing.Size(152, 22);
+            this.New.Size = new System.Drawing.Size(114, 22);
             this.New.Text = "New";
             // 
             // Open
             // 
             this.Open.Name = "Open";
-            this.Open.Size = new System.Drawing.Size(152, 22);
+            this.Open.Size = new System.Drawing.Size(114, 22);
             this.Open.Text = "Open";
             this.Open.Click += new System.EventHandler(this.Open_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(111, 6);
             // 
             // Save
             // 
             this.Save.Name = "Save";
-            this.Save.Size = new System.Drawing.Size(152, 22);
+            this.Save.Size = new System.Drawing.Size(114, 22);
             this.Save.Text = "Save";
             this.Save.Click += new System.EventHandler(this.Save_Click);
             // 
             // SaveAs
             // 
             this.SaveAs.Name = "SaveAs";
-            this.SaveAs.Size = new System.Drawing.Size(152, 22);
+            this.SaveAs.Size = new System.Drawing.Size(114, 22);
             this.SaveAs.Text = "Save As";
             this.SaveAs.Click += new System.EventHandler(this.SaveAs_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(111, 6);
             // 
             // Exit
             // 
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(152, 22);
+            this.Exit.Size = new System.Drawing.Size(114, 22);
             this.Exit.Text = "Exit";
             this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
@@ -135,6 +174,14 @@
             this.toolStripButton1.Text = "Help";
             this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(393, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 30);
+            this.textBox1.TabIndex = 4;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -144,6 +191,8 @@
             this.Controls.Add(this.panel1);
             this.Name = "Main";
             this.Text = "Classroom Robots";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -164,6 +213,10 @@
         private System.Windows.Forms.ToolStripMenuItem New;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Label Class_Label;
+        private System.Windows.Forms.TextBox Teacher_Text;
+        private System.Windows.Forms.Label Teacher_Label;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
